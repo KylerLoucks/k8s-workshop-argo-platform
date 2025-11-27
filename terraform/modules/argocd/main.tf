186f33bc3b8d6e2af228cda9da2ace7dfb4fbef8
KylerLoucks <kyler@loucksland.com>
2025-11-26 16:38:47 -0800

    docs: updated comments

--- terraform/modules/argocd/main.tf
+++ terraform/modules/argocd/main.tf
@@ -219,7 +219,7 @@ resource "argocd_application" "app_of_apps" {
 
 ################################################################################
 # ArgoCD Repositories
-# Generates secret in ArgoCD namespace for access to private git repositories
+# Generates secret in ArgoCD namespace for access to private git/helm repositories
 ################################################################################
 resource "argocd_repository" "repository" {
   for_each = var.create ? var.repositories : {}
